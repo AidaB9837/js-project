be4e5f742ec2ba146335b7064be3c10d9a7e726e
Sara Esu <saraesu@gmail.com>
2022-05-12 16:22:11 +0200

    feat: add cookies

--- page.js
+++ page.js
@@ -6,5 +6,13 @@
 
 
 /* ----------------------------- COOKIES ----------------------------- */
-
-document.cookie = encodeURIComponent()
+let allCookies=document.querySelector('#all-cookies')
+let fewCookies=document.querySelector('#few-cookies')
+allCookies.addEventListener('click',()=>{
+    document.getElementsByClassName("cookies")[0].style.display="none";
+    console.log("L'utente accetta tutti i cookies")
+})
+fewCookies.addEventListener('click',()=>{
+    document.getElementsByClassName("cookies")[0].style.display="none";
+    console.log("L'utente accetta solo i cookies necessari")
+})
